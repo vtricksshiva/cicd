a7c55b9939e1b0bf5fb410bac3748b698e10bc76
vtricksshiva <shivanand@vtrickstech.com>
2025-11-26 23:00:01 +0530

    l

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "CI/CD Deployment Successful! Welcome to the Demo App."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
